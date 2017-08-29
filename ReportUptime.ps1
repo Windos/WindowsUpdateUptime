5d6ad056b2a09d1cc0fc0c291f09af88fba8ce6d
Windos (Auto) <windosbk+auto@gmail.com>
2017-08-30 08:10:58 +1200

    Update VM Uptime - 30/08/2017 8:10 AM

--- ReportUptime.ps1
+++ ReportUptime.ps1
@@ -9,11 +9,23 @@ foreach ($Line in $Content) {
         $Pro1Uptime = '<p id="Win10-Pro-LoggedIn-GPO">' + $Pro1.Days + ' days, ' + $Pro1.Hours + ' hours, ' + $Pro1.Minutes + ' minutes</p>'
         $Content = $Content -replace $Line, $Pro1Uptime
     }
+
+    if ($Line -like '*Win10-Pro-LoggedOut-REG*') {
+        $Pro2 = (Get-VM -Name Win10-Pro-LoggedOut-REG).Uptime
+        $Pro2Uptime = '<p id="Win10-Pro-LoggedOut-REG">' + $Pro2.Days + ' days, ' + $Pro2.Hours + ' hours, ' + $Pro2.Minutes + ' minutes</p>'
+        $Content = $Content -replace $Line, $Pro2Uptime
+    }
     
     if ($Line -like '*Win10-Ent-LoggedIn-GPO*') {
         $Ent1 = (Get-VM -Name Win10-Ent-LoggedIn-GPO).Uptime
         $Ent1Uptime = '<p id="Win10-Ent-LoggedIn-GPO">' + $Ent1.Days + ' days, ' + $Ent1.Hours + ' hours, ' + $Ent1.Minutes + ' minutes</p>'
         $Content = $Content -replace $Line, $Ent1Uptime
+    }
+
+    if ($Line -like '*Win10-Ent-LoggedOut-REG*') {
+        $Ent2 = (Get-VM -Name Win10-Ent-LoggedOut-REG).Uptime
+        $Ent2Uptime = '<p id="Win10-Ent-LoggedOut-REG">' + $Ent2.Days + ' days, ' + $Ent2.Hours + ' hours, ' + $Ent2.Minutes + ' minutes</p>'
+        $Content = $Content -replace $Line, $Ent2Uptime
     }
 }
 
